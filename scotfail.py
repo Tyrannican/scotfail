4bc9da8dfd9159a4513eb3bc1b494634212158c4
Graham Keenan <grahamkeenan@hotmail.co.uk>
2018-12-09 18:59:51 +0000

    Fuck scotrail

--- scotfail.py
+++ scotfail.py
@@ -1,14 +1,36 @@
+"""Small hacky script to check timetables for scotrail
+Utilises the API supplied by https://github.com/rtgnx/scotrail-api
+"""
+
 import json
 import requests
 
-URL = "https://scotraiL.pw/live/"
+# API location
+URL = "https://scotrail.pw/live/"
 
 
 def read_code_file():
+    """Reads the JSON file for station codes
+    
+    Returns:
+        dict -- List of station codes and the station names
+    """
     with open("codes.json") as f_d:
         return json.load(f_d)
 
+
 def get_response(station_code):
+    """Calls the API with the Station code to get the info
+    
+    Arguments:
+        station_code {str} -- Three letter station code
+    
+    Raises:
+        Exception -- Not a 200 response from the API
+    
+    Returns:
+        dict -- Response as JSON
+    """
     target = URL + station_code
     resp = requests.get(target)
 
@@ -19,6 +41,16 @@ def get_response(station_code):
 
 
 def get_services(station_code, destination):
+    """Rips out the services from the response from the API
+    
+    Arguments:
+        station_code {str} -- Three letter station code
+        destination {str} -- Three letter code for destination station (Optional)
+    
+    Returns:
+        list -- List of services from the Station
+    """
+
     live = get_response(station_code)
     services = live["services"]
 
@@ -28,6 +60,19 @@ def get_services(station_code, destination):
 
 
 def get_info(station_code, destination=None):
+    """Builds info dict for services from a station
+    Passing in a destination gives services going to that station
+    
+    Arguments:
+        station_code {str} -- Three letter station code for target station
+    
+    Keyword Arguments:
+        destination {str} -- Three letter station code for destination (Optional)
+    
+    Returns:
+        dict -- Services from that station
+    """
+
     services = get_services(station_code, destination)
     service_info = {}
     for service in services:
@@ -44,6 +89,15 @@ def get_info(station_code, destination=None):
 
 
 def enquire(sc, dest=None):
+    """Query the API for train information
+    
+    Arguments:
+        sc {str} -- Three letter station code for source station
+    
+    Keyword Arguments:
+        dest {str} -- Three letter station code for destination (Optional)
+    """
+
     if not dest:
         info = get_info(sc)
     else:
